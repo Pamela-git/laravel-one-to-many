8948c6f21939cf8b1e64d3e2d0ac535c2e8b1574
Pamela Ladu <pamela.ladu@gmail.com>
2021-02-04 16:01:07 +0100

    model compilati

---
+++ laravel1tomany/app/Employee.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+  protected $fillable = [
+    'name',
+    'lastname',
+    'date_of_birth',
+  ];
+
+  public function tasks()
+  {
+    return $this -> hasMany(Task::class);
+  }
+}
